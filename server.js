9a153e42f0344edf5b31615a1cd65f8bce55f085
Janardhana <64420272+Janardhan242@users.noreply.github.com>
2022-07-24 17:24:12 +0530

    updated
    updated

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ const HOST='0.0.0.0';
 const app=express();
 app.get('/',(req,res))=>{
 res.send('Hello World\n');
-});
+};
 app.listen(PORT,HOST);
 console.log('Running on https://${HOST}:${PORT}');
